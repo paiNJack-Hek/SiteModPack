bfcec5e204b753062f5c6e64dfc23b50d3c8a7ca
paiNJack-Hek <painjackformy24@gmail.com>
2024-06-12 18:48:05 +0300

    Update script.js
    document.getElementById('downloadButton').addEventListener('click', function() {
        alert('Мод-пак успешно скачан!');
        // Здесь вы можете добавить код для реальной загрузки мод-пака
    });

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.getElementById('downloadButton').addEventListener('click', function() {
+    alert('Мод-пак успешно скачан!');
+    // Здесь вы можете добавить код для реальной загрузки мод-пака
+});
